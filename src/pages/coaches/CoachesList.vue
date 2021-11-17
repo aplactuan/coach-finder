6886405162756f84a619d4ed5167eee42d9fc91d
Adrian Lactuan <aplactuan@gmail.com>
2021-11-17 15:54:49 +0800

    Coach Item component, Coach List Component styling

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -8,16 +8,28 @@
             <router-link to="/register">Register as Coach</router-link>
         </div>
         <ul v-if="hasCoaches">
-            <li v-for="coach in filteredCoaches" :key="coach.id">
-                {{ coach.firstName }}
-            </li>
+            <coach-item 
+                v-for="coach in filteredCoaches" 
+                :key="coach.id"
+                :firstName="coach.firstName"
+                :lastName="coach.lastName"
+                :rate="coach.hourlyRate"
+                :areas="coach.areas"
+                :id="coach.id"
+            >
+            </coach-item>
         </ul>
         <h3 v-else>No coaches found</h3>
     </section>
 </template>
 
 <script>
+import CoachItem from '../../components/coaches/CoachItem.vue';
+
 export default {
+    components: {
+        CoachItem
+    },
     computed: {
         filteredCoaches() {
             return this.$store.getters['coaches/coaches'];
@@ -28,3 +40,16 @@ export default {
     }
 }
 </script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.controls {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
