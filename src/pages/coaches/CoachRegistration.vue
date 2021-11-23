ba7e69a4f45cd2110b0da42b30139bf1e706cf13
Adrian Lactuan <aplactuan@gmail.com>
2021-11-23 16:07:45 +0800

    Coach Form Registration Component

--- src/pages/coaches/CoachRegistration.vue
+++ src/pages/coaches/CoachRegistration.vue
@@ -1,3 +1,18 @@
 <template>
-    Register
+    <section>
+        <base-card>
+            <h2>Register as a Coach Now!</h2>
+            <coach-form></coach-form>
+        </base-card>
+    </section>
 </template>
+
+<script>
+import CoachForm from '../../components/coaches/CoachForm.vue';
+
+export default {
+    components: {
+        CoachForm
+    }    
+}
+</script>
