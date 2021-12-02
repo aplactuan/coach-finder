7ab08e402a188c7e9e4ed5633464b9abc7177bde
Adrian Lactuan <aplactuan@gmail.com>
2021-12-02 18:24:44 +0800

    Store message to vuex and output it in the component

--- src/pages/requests/RequestReceived.vue
+++ src/pages/requests/RequestReceived.vue
@@ -1,3 +1,54 @@
 <template>
-    Request receive
+  <section>
+    <base-card>
+      <header>
+        <h2>Received Request</h2>
+      </header>
+      <ul v-if="hasRequests">
+        <request-item
+          v-for="request in receiveRequests"
+          :email="request.userEmail"
+          :message="request.message"
+          :key="request.id"
+        >
+        </request-item>
+      </ul>
+      <h3 v-else>You have'nt receive any requests yet</h3>
+    </base-card>
+  </section>
 </template>
+
+<script>
+import RequestItem from '../../components/requests/RequestItem.vue';
+
+export default {
+  components: {
+    RequestItem,
+  },
+  computed: {
+    receiveRequests() {
+      return this.$store.getters['requests/requests'];
+    },
+    hasRequests() {
+      return this.$store.getters['requests/hasRequests'];
+    },
+  },
+};
+</script>
+
+<style scoped>
+header {
+  text-align: center;
+}
+
+ul {
+  list-style: none;
+  margin: 2rem auto;
+  padding: 0;
+  max-width: 30rem;
+}
+
+h3 {
+  text-align: center;
+}
+</style>
--- src/store/modules/request/actions.js
+++ src/store/modules/request/actions.js
@@ -1,7 +1,7 @@
 export default {
     contactCoach(context, payload) {
         const newRequest = {
-            id: new Date().toISOString,
+            id: new Date().toISOString(),
             coachId: payload.coachId,
             userEmail: payload.email,
             message: payload.message
