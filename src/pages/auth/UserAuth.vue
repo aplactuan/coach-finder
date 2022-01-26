aa8df21decb5112eaaa62b5770f9746725bc5651
Adrian Lactuan <aplactuan@gmail.com>
2022-01-26 15:14:42 +0800

    Adding signup loading spinner and add login logic

--- src/pages/auth/UserAuth.vue
+++ src/pages/auth/UserAuth.vue
@@ -1,19 +1,27 @@
 <template>
-    <base-card>
-        <form @submit.prevent="submitForm">
-            <div class="form-control">
-                <label for="email">Email</label>
-                <input type="text" name="email" id="email" v-model.trim="email" />
-            </div>
-            <div class="form-control">
-                <label for="password">Password</label>
-                <input type="text" name="password" id="password" v-model.trim="password"/>
-            </div>
-            <base-button>{{ loginButtonCaption }}</base-button>
-            <base-button type="button" mode="flat" @click="switchAuthMode">{{ switchAuthCaption }}</base-button>
-            <p v-if="!formIsValid">Please enter a valide email and password</p>
-        </form>
-    </base-card>
+    <div>
+        <base-dialog :show="!!error" title="Failed" @close="handleError">
+            {{ error }}
+        </base-dialog>
+        <base-dialog :show="isLoading" title="Authenticating" fixed>
+            <base-spinner></base-spinner>
+        </base-dialog>
+        <base-card>
+            <form @submit.prevent="submitForm">
+                <div class="form-control">
+                    <label for="email">Email</label>
+                    <input type="text" name="email" id="email" v-model.trim="email" />
+                </div>
+                <div class="form-control">
+                    <label for="password">Password</label>
+                    <input type="password" name="password" id="password" v-model.trim="password"/>
+                </div>
+                <base-button>{{ loginButtonCaption }}</base-button>
+                <base-button type="button" mode="flat" @click="switchAuthMode">{{ switchAuthCaption }}</base-button>
+                <p v-if="!formIsValid">Please enter a valide email and password</p>
+            </form>
+        </base-card>
+    </div>
 </template>
 
 <script>
@@ -23,8 +31,10 @@ export default {
             email: '',
             password: '',
             formIsValid: true,
-            mode: 'login'
-        }
+            mode: 'login',
+            isLoading: false,
+            error: null
+        };
     },
     computed: {
         loginButtonCaption() {
@@ -41,22 +51,32 @@ export default {
         }
     },
     methods: {
-        submitForm() {
+        async submitForm() {
             this.formIsValid = true;
             if (this.email === '' || !this.email.includes('@') || this.email.length < 6) {
                 this.formIsValid = false;
                 return;
             }
 
-            if (this.mode === 'login') {
-                return;
+            this.isLoading = true;
+
+            const actionPayload = {
+                        email: this.email,
+                        password: this.password
+                    };
+
+            try {
+                if (this.mode === 'login') {
+                     await this.$store.dispatch('login', actionPayload);
+                }
+                else {
+                    await this.$store.dispatch('signup', actionPayload);
+                }
             }
-            else {
-                this.$store.dispatch('signup', {
-                    email: this.email,
-                    password: this.password
-                });
+            catch (e) {
+                this.error = e.message || 'Failed';
             }
+            this.isLoading = false;
         },
         switchAuthMode() {
             if (this.mode === 'login') {
@@ -65,6 +85,9 @@ export default {
             else {
                 this.mode = 'login';
             }
+        },
+        handleError() {
+            this.error = null;
         }
     }
 }
